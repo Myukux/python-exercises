bace3844d4aee2946dae2222e985a1b4d6e94f03
AHercog <anubarak123@gmail.com>
2018-07-13 22:37:20 +0200

    start ex7

--- exercises/ex7.py
+++ exercises/ex7.py
@@ -1,3 +1,10 @@
-a=input("podaj dwa numery odzielajac je przecinkami: ")
-a=a.split(",")
-for i in range
+a = input("podaj dwa numery odzielajac je przecinkami: ")
+a = a.split(",")
+c = []
+b = int(a[0])
+d=int(a[1])
+for i in range(0, d):
+    c.append([])
+    for j in range(0,b):
+        r = i * j
+        print(c)
