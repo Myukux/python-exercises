43416be4ca7c3b2e36b00f6fc90ae5f4aacc55c4
AHercog <anubarak123@gmail.com>
2018-07-14 19:20:57 +0200

    finish ex7

--- exercises/ex7.py
+++ exercises/ex7.py
@@ -2,9 +2,11 @@ a = input("podaj dwa numery odzielajac je przecinkami: ")
 a = a.split(",")
 c = []
 b = int(a[0])
-d=int(a[1])
-for i in range(0, d):
+d = int(a[1])
+for i in range(0, b):
     c.append([])
-    for j in range(0,b):
+    for j in range(0, d):
         r = i * j
-        print(c)
+        c[i].append(r)
+
+print(c)
