93ee8a367b4e998776ddb979f71ee71b36d8ff9d
AHercog <anubarak123@gmail.com>
2018-08-11 02:21:45 +0200

    finish ex10

---
+++ exercises/ex10.py
@@ -0,0 +1,28 @@
+
+
+# a = input("Podaj słowa odzielając je spacją: ")
+# a = a.split(" ")
+# c = []
+# for i in a:
+#     if i not in c:
+#         c.append(i)
+# a = sorted(a)
+# a = " ".join(a)
+# print(a)
+
+a = input("Podaj słowa odzielając je spacją: ")
+a = a.split(" ")
+c = []
+for i in a:
+    does_exist_in_list = False
+    for j in c:
+        if i != j:
+            does_exist_in_list = False
+        else:
+            does_exist_in_list = True
+            break
+    if not does_exist_in_list:
+        c.append(i)
+c = sorted(c)
+c = " ".join(c)
+print(c)
