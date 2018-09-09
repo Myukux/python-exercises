86169a10a8c76e114cde75881de165818ee62917
Myukux <ange96.wb@gmail.com>
2018-09-09 22:16:17 +0200

    Finish ex13

---
+++ exercises/ex13.py
@@ -0,0 +1,17 @@
+input_string = "abc def abc !123m543p?"
+
+letter_count = 0
+number_count = 0
+
+for letter in input_string:
+    if letter.isdigit():
+        number_count += 1
+    if letter.isalpha():
+        letter_count += 1
+
+print(letter_count)
+print(number_count)
+
+# for i in a:
+#     i.isdigit()
+#     i.isalpha()
